b0a0e6f7701b2e0555503adfc4a4a42e473d203d
y2kpartytime <C24732831@myTUDublin.ie>
2025-04-24 00:01:48 +0100

    Test
    Test

--- Player/PlayerScript.gd
+++ Player/PlayerScript.gd
@@ -2,28 +2,30 @@ extends CharacterBody2D
 
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var dash_timer: Timer = $DashTimer
+@onready var dash_again_timer: Timer = $DashAgain
+
 
 
 const GRAVITY = 1000
 const SPEED = 300 
 const Jump = -300
 const Jump_horizontal = 100
+const dash_speed = 900
 
 enum State { Idle, Run, Jump, Shoot }
+var dash_dir = 0
 
 var current_state : State
 var muzzle_position
 var shoot_timer := 0.0  
+var is_sb = false
+var can_sb = true
 
 func _ready():
 	current_state = State.Idle
-	
-
 
 func _physics_process(delta):
-
-
-	
 	if shoot_timer <= 0:
 		player_falling(delta)
 		player_Idle(delta)
@@ -33,6 +35,16 @@ func _physics_process(delta):
 		shoot_timer -= delta
 		if shoot_timer <= 0:
 			current_state = State.Idle
+
+	if Input.is_action_just_pressed("dash") and can_sb:
+		print("Dashed!")
+		is_sb = true
+		can_sb = false
+		dash_timer.start()
+		dash_again_timer.start()
+
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
 
 	move_and_slide()
 	player_animations()
@@ -48,25 +60,27 @@ func player_Idle(delta):
 
 func player_run(delta):
 	var direction = Input.get_axis("move_left", "move_right")
-	
 	if direction:
-		velocity.x = direction * SPEED
+		if is_sb:
+			velocity.x = direction * dash_speed
+			velocity.y = 0
+		else:
+			velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
 	if direction != 0:
 		current_state = State.Run
 		animated_sprite_2d.flip_h = false if direction > 0 else true
 
+
 func player_jump(delta):
-	if Input.is_action_just_pressed("Jump"):
+	if Input.is_action_just_pressed("jump"):
 		velocity.y = Jump
 		current_state = State.Jump
 		
 	if !is_on_floor() and current_state == State.Jump:
 		var direction = Input.get_axis("move_left", "move_right")
 		velocity.x += direction * Jump_horizontal * delta 
-
 
 func player_animations():
 	if current_state == State.Idle:
@@ -86,3 +100,10 @@ func player_animations():
 			#animated_sprite_2d.play("Jump")
 		#State.Shoot:
 			#animated_sprite_2d.play("Shot1")
+
+
+func _on_dash_timer_timeout() -> void:
+	is_sb = false
+
+func _on_dash_again_timeout() -> void:
+	can_sb = true
